8406830f3873898a9cc91e59f8dcdf66b8faa9db
imarkov <imarkov@vmware.com>
2021-07-21 11:19:09 +0300

    Cleanup; bugfixing - #[cfg(espXX)] works now

--- build.rs
+++ build.rs
@@ -1,4 +1,5 @@
 // Necessary because of this issue: https://github.com/rust-lang/cargo/issues/9641
 fn main() -> anyhow::Result<()> {
+    println!("cargo:rustc-cfg={}", std::env::var("DEP_ESP_IDF_MCU").unwrap());
     pio::cargo::build::LinkArgs::output_propagated("ESP_IDF")
 }
