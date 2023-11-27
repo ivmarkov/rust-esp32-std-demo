7aac421c587f2f83e874d01bb5d04928464ac497
Luca Barbato <lu_zero@gentoo.org>
2023-11-27 14:08:27 +0100

    Update the qemu.sh to the current cargo espflash
    It accepts only `mb` and not `M` as unit.

--- qemu.sh
+++ qemu.sh
@@ -6,5 +6,5 @@ BUILD=debug
 
 TARGET=xtensa-esp32-espidf # Don't change this. Only the ESP32 chip is supported in QEMU for now
 
-cargo espflash save-image --features qemu --merge target/$TARGET/$BUILD/rust-esp32-std-demo.bin --chip esp32 -s 4M -T partitions.csv
+cargo +esp espflash save-image --features qemu --merge target/$TARGET/$BUILD/rust-esp32-std-demo.bin --chip esp32 -s 4mb -T partitions.csv
 $ESP_QEMU_PATH/qemu-system-xtensa -nographic -machine esp32 -nic user,model=open_eth,id=lo0,hostfwd=tcp:127.0.0.1:7888-:80 -drive file=target/$TARGET/$BUILD/rust-esp32-std-demo.bin,if=mtd,format=raw
